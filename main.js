714d8e0f5b26f49495b555270b3921239b4b4477
Vad1kRed <totalgol2015@gmail.com>
2021-09-02 14:14:24 +0300

    Task 2 - :white_check_mark:

--- main.js
+++ main.js
@@ -1,9 +1,9 @@
 // Task 1
 while (true) {
+    const form = document.querySelector('form');
     const number = Math.floor(prompt('Введите количество input:', 1));
-
+    
     for (let i = number; i > 0; i--) {
-        const form = document.querySelector('form');
         const input = document.createElement('input');
     
         input.setAttribute('type', 'text');
@@ -28,3 +28,29 @@ while (true) {
 
     if (!isNaN(number) && number > 0) break;
 }
+// Task 2
+const time = document.createElement('div');
+const h2 = document.createElement('h2');
+const btnStop = document.createElement('button');
+const btnStart = document.createElement('button');
+
+time.classList.add('time');
+btnStop.textContent = 'Stop';
+btnStart.textContent = 'Start';
+
+document.body.append(time);
+time.prepend(h2, btnStop, btnStart);
+
+const getTime = () => {
+    h2.textContent = new Date().toLocaleTimeString();
+}
+
+btnStart.addEventListener('click', () => {
+    const intervalId = setInterval(getTime, 1000);
+
+    btnStop.addEventListener('click', () => {
+        clearInterval(intervalId);
+    })
+})
+
+getTime();
