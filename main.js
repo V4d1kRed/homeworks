483e8fdd96efecac2ca0b2dbd79a1cfcba2f4b9b
Vad1kRed <totalgol2015@gmail.com>
2021-09-07 00:58:11 +0300

    Task 2 - done

--- main.js
+++ main.js
@@ -21,3 +21,37 @@ promise
     }
   })
   .catch(error => console.log(error));
+// Task 2
+const vegetables = ['Potato', 'Tomato', 'Carrot', 'Cabbage', 'Pepper', 'Hot', 'Eggplant', 'Cauliflower'];
+
+function goToShop(arr) {
+  return new Promise((resolve, reject) => resolve(arr.length));
+}
+
+function makeDinner() {
+  return new Promise((resolve, reject) => {
+    setInterval(() => {
+      resolve('Bon Appetit');
+    }, 3000);
+  });
+}
+
+goToShop(vegetables)
+  .then(length => {
+    if (length >= 4) {
+      makeDinner()
+        .then(response => console.log(response))
+    } else {
+      return new Promise((resolve, reject) => reject('Too low products'));
+    }
+  })
+  .catch(error => {
+    throw new ValidationError(error);
+  })
+
+class ValidationError extends Error {
+  constructor(message) {
+    super(message);
+    this.name = 'Product Error';
+  }
+}
